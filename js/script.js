65928a65d33de5c86f15811657827bacacaa7763
Andrea <andreagn7@gmail.com>
2022-11-19 22:54:38 +0100

    actualización 19-11

--- js/script.js
+++ js/script.js
@@ -1,69 +1,176 @@
 'use strict';
 
-let contador = 0;
-let displayRGB = document.querySelector('#displayRGB');
+const h2 = document.querySelector('h2');
+const p = document.querySelector('p');
+const displayRGB = document.querySelector('#displayRGB');
 const reset = document.querySelector('#reset');
-let message = document.getElementById('message');
-const intentos = 6;
-const div = document.querySelector('div#container');
-const circles = 4;
-let square = document.querySelectorAll('div#container div');
-let h2 = document.querySelector('h2');
-let p = document.querySelector('p');
+const message = document.getElementById('message');
 
-// console.log(square); muestra ok todos los div con class
+const squares = document.querySelectorAll('div');
 
 
-// ya he creado la funcioan manejadora del evento y la he 
-// añadido al boton de "quiero 
-// mas colores". Le he puesto una clase para que sepamos que 
-// funciona.
 
-function resetClickHandler() {
-     console.log('click');
-     reset.style.backgroundColor = '#0a0a0a';
-     reset.style.color = '#ffffff';
-};
-
-// la funcion de arriba, esta llamada aqui abajo 
-//(resetClikHandler)
-reset.addEventListener('click', resetClickHandler);
-reset.addEventListener('click', h2ClickHandler);
-reset.addEventListener('click', pClickHandler);
-
-// He creado tambien como eliminarlo, para probar si funciona
-// que con remove, vuelva otra vez al inicio. Lo voy a 
-// dejar comentado, ya que nos puede venir bien mas 
-// adelante.
-//reset.removeEventListener ('click', resetClickHandler);
+function coloresAlea () {
+     
+     const arrayColors = []
  
+     
+     for(let i=0; i<=2; i++){
+         const colorAleatorio = Math.round(Math.random () * 255);
+         arrayColors.push(colorAleatorio);
+     }
+ 
+     const id = Math.ceil(Math.random() * 4); 
+ 
+   
+     arrayColors.push(id);
+ 
+    
+     return arrayColors;
+ }
+ console.log(coloresAlea());
+ let colorAdivinar= coloresAlea();
 
-// Funcion manejadora para que cuando se haga click en el
-// ciculo.
-// Lo he hecho con la clase del Bubbling. Cuando se hace 
-// click en en todo el DIV, se genera el evento, no solo en 
-// el circulo.
-div.addEventListener('click', () => {
-     console.log('click en el circulo');
-});
+ 
+ function modificarColorOriginal(arrayColores){
+     const colorModificado =[]
+     for( const color of arrayColores){
+         const numAlea =Math.round(Math.random() * 50)
+         if(color > 205){
+             
+             colorModificado.push(color- numAlea);
+         }else{
+             colorModificado.push(color+ numAlea);
+         }
+     }
+ 
+     return colorModificado;
+ }
+ 
+ 
+ 
+ reset.addEventListener('click', ()=>{
+     let colorAdivinar= coloresAlea(); 
+     p.textContent=`RGB(${colorAdivinar[0]}, ${colorAdivinar[1]}, ${colorAdivinar[2]})`;
+     p.style.backgroundColor=`RGB(${colorAdivinar[0]}, ${colorAdivinar[1]}, ${colorAdivinar[2]})`;
+     modificarColorOriginal(colorAdivinar);
+     fallos.textContent='Fallos:';
+     aciertos.textContent='Aciertos:';
 
+     message.textContent='';
+ 
+     
+ 
+     for (const cuadrado of squares) {
+         
+         let modifyColor = modificarColorOriginal(colorAdivinar);
+ 
+         cuadrado.style.backgroundColor = `RGB(${modifyColor[0]}, ${modifyColor[1]}, ${modifyColor[2]})`;
+ 
+         
+         
+         if (cuadrado.id === colorAdivinar[3]) {
+            
+             cuadrado.style.backgroundColor = `RGB(${colorAdivinar[0]}, ${colorAdivinar[1]}, ${colorAdivinar[2]})`;
+         }
+     }
+     console.log('click en jugar');
+})
 
-function getRandomColor (){
-     const randomColor = Math.round(Math.random () * 255);
-     return randomColor;
+const square1 = document.getElementById('1');
+const square2 = document.getElementById('2');
+const square3 = document.getElementById('3');
+const square4 = document.getElementById('4');
 
-};
+const allSquares = [square1, square2, square3, square4];
 
-p.textContent = `RGB(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+//const contador = document.getElementById('contador');
+const aciertos = document.getElementById('aciertos');
+const fallos = document.getElementById('fallos');
 
-function h2ClickHandler (){
-     h2.style.backgroundColor = `RGB(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
-};
-h2.addEventListener('click', h2ClickHandler);
+let contador =0;
+let intentos = 5;
 
-function pClickHandler(){
-     p.textContent = `RGB(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+function squareClickHandler(event){
+    //message.textContent=`Te quedan ${intentos} intentos`
+    square1.addEventListener('click', ()=>{
+    
+        if(square1.style.backgroundColor === colorAdivinar[3]){
+            console.log('square1 HAS ACERTADO');
+        }else{
+            console.log('square1 HAS FALLADO');
+        }
+    })
+    
+    square2.addEventListener('click', ()=>{
+        
+        if(square2.style.backgroundColor === colorAdivinar[3]){
+            console.log('square2 HAS ACERTADO');
+        }else{
+            console.log('square2 HAS FALLADO');
+        }
+    })
+    
+    square3.addEventListener('click', ()=>{
+        
+        if(square3.style.backgroundColor === colorAdivinar[3]){
+            console.log('square3 HAS ACERTADO');
+        }else{
+            console.log('square3 HAS FALLADO'); 
+        }
+    })
+    
+    square4.addEventListener('click', ()=>{
+        
+        if(square4.style.backgroundColor === colorAdivinar[3]){
+            console.log('square4 HAS ACERTADO');
+        }else{
+            console.log('square4 HAS FALLADO');   
+        }
+    })
+
+    
+    
+    if(event.target.id === colorAdivinar[3]){
+        contador++;
+        intentos--;
+        aciertos.textContent = `Aciertos: ${contador}`;
+    
+    
+    }else{
+        contador++;
+        intentos--;
+        fallos.textContent = `Fallos: ${contador}`;
+    
+    }
+        
+    for(let i=0; i<=2; i++){
+        
+        if(fallos === 3){
+            fallos.textContent='HAS PERDIDO';
+            console.log(`Has perdido`);
+            contador=0;
+            break;
+        }if(aciertos === 3){
+            aciertos.textContent='HAS GANADO';
+            contador=0;
+            break;
+        }else if(contador === 3){
+            contador=0;
+            intentos = 0;
+            break;
+        }
+    }
+    
+    for(let i=6; i>=0; i--){
+        if(intentos === 0){
+            break;
+        }
+        break
+    }
 }
-p.addEventListener('click', pClickHandler);
 
-
+square1.addEventListener('click', squareClickHandler);
+square2.addEventListener('click', squareClickHandler);
+square3.addEventListener('click', squareClickHandler);
+square4.addEventListener('click', squareClickHandler);
